c24904e27fc3eb117580824ff51614b6fe5cd0c2
mingming <394227933@qq.com>
2020-03-23 23:13:12 +0800

    学习Linux之后第一次Git代码

---
+++ index.js
@@ -0,0 +1,9 @@
+var People = /** @class */ (function () {
+    function People(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    return People;
+}());
+var a = new People('xiwang', 18);
+console.log(a);
---
+++ index.ts
@@ -0,0 +1,10 @@
+class People {
+    public name:string
+    public age:number
+    constructor(name,age){
+        this.name=name
+        this.age=age
+    }
+}
+let a=new People('xiwang',18)
+console.log(a)
